b2e526ad4f84917e09faf5c6f0668f2f3352ad72
osx user <gili.shemesh@gmail.com>
2020-12-10 23:34:30 +0200

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import './plugins/composition-api'
 import App from './App.vue'
 import router from './router'
 
