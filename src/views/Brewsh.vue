98cd87fcb3d894d319c7e53bf21ce9fdd6c91c25
osx user <gili.shemesh@gmail.com>
2020-12-12 19:58:09 +0200

    commit

---
+++ src/views/Brewsh.vue
@@ -0,0 +1,14 @@
+<template>
+  <iframe src="https://brew.sh/" >
+</iframe>
+</template>
+
+<style scoped lang="scss">
+iframe {
+    display: block;       /* iframes are inline by default */
+    background: #000;
+    border: none;         /* Reset default border */
+    height: 100vh;        /* Viewport-relative units */
+    width: 100vw;
+}
+</style>
