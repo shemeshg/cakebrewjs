582c6d4f6f009de81b09352c9d4307ccb6bde80a
osx user <gili.shemesh@gmail.com>
2020-12-12 19:57:43 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/info">Info</router-link> |
+      <router-link to="/brewsh">://brew.sh</router-link> |      
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
