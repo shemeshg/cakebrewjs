38a0dfcb0c4c6206841b45a0c6a6825bbb2021fb
osx user <gili.shemesh@gmail.com>
2020-12-16 07:33:57 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,32 @@
     <div id="nav" v-if="isShowNavigation">
       <router-link to="/">Home</router-link> |
       <router-link to="/info">Info</router-link> |
-      <router-link to="/brewsh">://brew.sh</router-link> |      
+      <b-link href="#"  @click="reouteBrewSh">://brew.sh</b-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 <script lang="ts">
- import { computed, inject  } from "@vue/composition-api";
+import { computed, inject } from "@vue/composition-api";
+
 export default {
   /* eslint-disable */
   setup(prop: any, ctx: any) {
-   
-      // eslint-disable-next-line
+    // eslint-disable-next-line
     const store: any = inject("vuex-store");
     // eslint-disable-next-line
     const isShowNavigation = computed(() => store.state.isShowNavigation);
 
-    return {isShowNavigation}
-  }
-}
+    function reouteBrewSh (){
+      const { shell } = require("electron");
+      shell.openExternal("https://brew.sh");
+    }
+
+    return { isShowNavigation, reouteBrewSh };
+  },
+};
 </script>
 
 <style lang="scss">
-
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -17,14 +17,6 @@ const routes: Array<RouteConfig> = [
     component: Info
   },  
   {
-    path: '/brewsh',
-    name: 'Brewsh',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Brewsh.vue')
-  },  
-  {
     path: '/about',
     name: 'About',
     // route level code-splitting
